d1e601822861060c56dc9789fa3f251d2e241076
subhadip420 <subhadippramanik02@gmail.com>
2026-08-24 11:59:04 +0530

    fixed warning in merge screen.

--- lib/screens/merge_screen.dart
+++ lib/screens/merge_screen.dart
@@ -305,7 +305,7 @@ class _MergeScreenState extends State<MergeScreen> {
                                         color: Colors.white,
                                         boxShadow: [
                                           BoxShadow(
-                                            color: Colors.black.withOpacity(0.5),
+                                            color: Colors.black.withValues(alpha:0.5),
                                             blurRadius: 10,
                                             spreadRadius: 2,
                                           ),
