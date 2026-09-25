d1c23cd8f0c3488fcb1c639e21f082d0a9aa1f6e
subhadip420 <subhadippramanik02@gmail.com>
2026-09-25 11:02:38 +0530

    Fixed Edge-to-edge Display issue.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -71,3 +71,7 @@ android {
 flutter {
     source = "../.."
 }
+
+dependencies {
+    implementation("androidx.activity:activity-ktx:1.9.0")
+}
--- android/app/src/main/kotlin/com/sptechstudios/pdf_scanner_pro/MainActivity.kt
+++ android/app/src/main/kotlin/com/sptechstudios/pdf_scanner_pro/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.sptechstudios.pdf_scanner_pro
 
-import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import io.flutter.embedding.android.FlutterFragmentActivity // Notice: FragmentActivity use kiya hai
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity() { // Notice: Yahan bhi FragmentActivity
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+    }
+}
